16bb91ba331921c834c8990e9b42dc3d95932f66
sondbj <sondrebj97-@hotmail.com>
2023-05-21 19:28:31 +0200

    yeah

---
+++ main.py
@@ -0,0 +1,3 @@
+from src.cloud_backend_cache.__main__ import app                                                               
+if __name__ == "__main__":                                             
+    app.run(debug=True)
